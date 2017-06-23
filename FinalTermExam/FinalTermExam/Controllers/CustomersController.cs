e0a3f4daa0bbcff2fabb3c0554d2e60013c8ca41
user <user@MA324-47>
2017-06-23 16:00:26 +0800

    0623 16:00

--- FinalTermExam/FinalTermExam/Controllers/CustomersController.cs
+++ FinalTermExam/FinalTermExam/Controllers/CustomersController.cs
@@ -8,11 +8,23 @@ namespace FinalTermExam.Controllers
 {
     public class CustomersController : Controller
     {
+        Models.Model1 db = new Models.Model1();
         // GET: Customers
         public ActionResult Index()
         {
-            var c = 15;
+                   
             return View();
+        }
+        
+        public ActionResult Search(Models.Customers c)
+        {
+            bool a = c.CompanyName == null;
+            var results = db.Customers
+                .Where(x => (c.CustomerID == 0) ? true : x.CustomerID == c.CustomerID)
+                .Where(x => (c.CompanyName == null) ? true : x.CompanyName.Contains(c.CompanyName))
+                .Where(x => (c.ContactName == null) ? true : x.ContactName.Contains(c.ContactName))
+                .Where(x => (c.ContactTitle  == null) ? true : x.ContactTitle == c.ContactTitle);
+            return Json(results, JsonRequestBehavior.AllowGet);
         }
     }
 }
